88de9bd050c97f60bda3484831d4103270dc434d
Franek Borowiec <franek.borowiec@gmail.com>
2020-05-12 11:44:16 +0200

    Single element in a sorted array solution

---
+++ 0540_single_element_in_a_sorted_array.cpp
@@ -0,0 +1,72 @@
+/**
+ * You are given a sorted array consisting of only integers where every element appears exactly twice,
+ * except for one element which appears exactly once. Find this single element that appears only once.
+ */
+#include <vector>
+
+class Solution {
+public:
+    int singleNonDuplicate(std::vector<int>& nums)
+    {
+      if (nums.size() < 2) return nums[0];
+      for (size_t i{0}; i<nums.size(); i=i+2)
+      {
+        if (nums[i+1] == nums[i]) continue;
+        else return nums[i];
+      }
+      return 0;
+    }
+};
+
+// TEST---------------------------------------------------------------------------------------------------------------|
+#include "gtest/gtest.h"
+
+namespace
+{
+
+TEST(SingleNonDuplicateTest, Number2_IsNonDuplicate)
+{
+  Solution sol{};
+  std::vector<int> input_vec{1, 1, 2, 3, 3, 4, 4, 8, 8};
+  int expected_result{2};
+
+  ASSERT_EQ(sol.singleNonDuplicate(input_vec), expected_result);
+}
+
+TEST(SingleNonDuplicateTest, Number10_IsNonDuplicate)
+{
+  Solution sol{};
+  std::vector<int> input_vec{3, 3, 7, 7, 10, 11, 11};
+  int expected_result{10};
+
+  ASSERT_EQ(sol.singleNonDuplicate(input_vec), expected_result);
+}
+
+TEST(SingleNonDuplicateTest, LastNumber_IsNonDuplicate)
+{
+  Solution sol{};
+  std::vector<int> input_vec{3, 3, 7, 7, 11, 11, 20};
+  int expected_result{20};
+
+  ASSERT_EQ(sol.singleNonDuplicate(input_vec), expected_result);
+}
+
+TEST(SingleNonDuplicateTest, FirstNumber_IsNonDuplicate)
+{
+  Solution sol{};
+  std::vector<int> input_vec{0, 3, 3, 7, 7, 11, 11, 20, 20};
+  int expected_result{0};
+
+  ASSERT_EQ(sol.singleNonDuplicate(input_vec), expected_result);
+}
+
+TEST(SingleNonDuplicateTest, OnlyOneNumber_ThenNumberIsNonDuplicate)
+{
+  Solution sol{};
+  std::vector<int> input_vec{1};
+  int expected_result{1};
+
+  ASSERT_EQ(sol.singleNonDuplicate(input_vec), expected_result);
+}
+
+}  // namespace
