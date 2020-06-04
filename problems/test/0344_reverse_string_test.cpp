09257bdcb2627be8811f69382d2691958f8f87ed
Franek Borowiec <franek.borowiec@gmail.com>
2020-06-04 10:04:40 +0200

    Reverse string

---
+++ problems/test/0344_reverse_string_test.cpp
@@ -0,0 +1,33 @@
+#include <limits>
+
+#include "gtest/gtest.h"
+
+#include "problems/0344_reverse_string.hpp"
+
+namespace {
+
+TEST(ReverseStringTest, helloBecomesolleh) {
+  reverse_string::Solution sol{};
+  std::vector<char> input = {'h', 'e', 'l', 'l', 'o'}, expected_result{'o', 'l', 'l', 'e', 'h'};
+
+  sol.reverseString(input);
+
+  ASSERT_EQ(input.size(), expected_result.size());
+  for (unsigned int i{0}; i < expected_result.size(); ++i) {
+    EXPECT_EQ(input[i], expected_result[i]);
+  }
+}
+
+TEST(ReverseStringTest, HannahBecomeshannaH) {
+  reverse_string::Solution sol{};
+  std::vector<char> input{'H', 'a', 'n', 'n', 'a', 'h'}, expected_result{'h', 'a', 'n', 'n', 'a', 'H'};
+
+  sol.reverseString(input);
+
+  ASSERT_EQ(input.size(), expected_result.size());
+  for (unsigned int i{0}; i < expected_result.size(); ++i) {
+    EXPECT_EQ(input[i], expected_result[i]);
+  }
+}
+
+}  // namespace
