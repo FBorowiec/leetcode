0fd56b758d37ccfe5ef4f95050f7fbc3c21738c3
Franek Borowiec <franek.borowiec@gmail.com>
2020-06-10 13:08:35 +0200

    Search insert position

---
+++ problems/test/0035_search_insert_position_test.cpp
@@ -0,0 +1,53 @@
+#include "gtest/gtest.h"
+
+#include "problems/0035_search_insert_position.hpp"
+
+namespace {
+
+using namespace search_insert_position;
+
+class SearchInsertPositionTest : public ::testing::Test {
+ public:
+  void SetUp() override {}
+
+ protected:
+  Solution sol;
+};
+
+TEST_F(SearchInsertPositionTest, 5isAtPos2) {
+  std::vector<int> input{1, 3, 5, 6};
+  int target{5};
+  int expected_result{2};
+
+  auto result = sol.searchInsert(input, target);
+  EXPECT_EQ(expected_result, result);
+}
+
+TEST_F(SearchInsertPositionTest, 2isAtPos1) {
+  std::vector<int> input{1, 3, 5, 6};
+  int target{2};
+  int expected_result{1};
+
+  auto result = sol.searchInsert(input, target);
+  EXPECT_EQ(expected_result, result);
+}
+
+TEST_F(SearchInsertPositionTest, 7isAtPos4) {
+  std::vector<int> input{1, 3, 5, 6};
+  int target{7};
+  int expected_result{4};
+
+  auto result = sol.searchInsert(input, target);
+  EXPECT_EQ(expected_result, result);
+}
+
+TEST_F(SearchInsertPositionTest, 0isAtPos0) {
+  std::vector<int> input{1, 3, 5, 6};
+  int target{0};
+  int expected_result{0};
+
+  auto result = sol.searchInsert(input, target);
+  EXPECT_EQ(expected_result, result);
+}
+
+}  // namespace
