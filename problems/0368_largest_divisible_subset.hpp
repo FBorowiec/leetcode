e2b5c4cbd8974549dd5aa8ffe5af8ecdaa5e2d03
Franek Borowiec <franek.borowiec@gmail.com>
2020-06-13 13:11:19 +0200

    Largest divisible subset template

---
+++ problems/0368_largest_divisible_subset.hpp
@@ -0,0 +1,24 @@
+// UNSOLVED!
+#ifndef LEETCODE_PROBLEMS_0368_LARGEST_DIVISIBLE_SUBSET_HPP
+#define LEETCODE_PROBLEMS_0368_LARGEST_DIVISIBLE_SUBSET_HPP
+
+#include <vector>
+
+namespace largest_divisible_subset {
+
+class Solution {
+ public:
+  /**
+   * Given a set of distinct positive integers,
+   * find the largest subset such that every pair (Si, Sj) of elements in this subset satisfies:
+   * Si % Sj = 0 or Sj % Si = 0.
+   * If there are multiple solutions, return any subset is fine.
+   */
+    std::vector<int> largestDivisibleSubset(std::vector<int>& nums) {
+        return {};
+    }
+};
+
+}  // namespace largest_divisible_subset
+
+#endif  // LEETCODE_PROBLEMS_0368_LARGEST_DIVISIBLE_SUBSET_HPP
