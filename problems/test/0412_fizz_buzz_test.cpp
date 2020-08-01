2cef9d6ceb6c5b004c8907099fdf5e5a5cf62f63
Franek Borowiec <franek.borowiec@gmail.com>
2020-08-01 21:47:48 +0200

    FizzBuzz

---
+++ problems/test/0412_fizz_buzz_test.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+
+#include "problems/0412_fizz_buzz.hpp"
+
+namespace {
+
+using namespace fizz_buzz;
+
+class FizzBuzz : public ::testing::Test {
+ public:
+  void SetUp() override {}
+
+ protected:
+  Solution sol{};
+  std::vector<std::string> expected_output{"1",    "2",    "Fizz", "4",    "Buzz", "Fizz", "7",       "8",
+                                           "Fizz", "Buzz", "11",   "Fizz", "13",   "14",   "FizzBuzz"};
+};
+
+TEST_F(FizzBuzz, FizzBuzzTest) {
+  auto result = sol.fizzBuzz(15);
+  for (unsigned int i{1}; i <= expected_output.size(); ++i) {
+    EXPECT_STREQ(expected_output[i].c_str(), result[i].c_str());
+  }
+}
+
+}  // namespace
